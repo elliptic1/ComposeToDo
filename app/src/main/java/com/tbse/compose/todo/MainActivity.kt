06f35152e7c2ee1e4611f4b4159c0008e2383491
Todd B. Smith <elliptic1@gmail.com>
2021-12-31 00:54:25 -0500

    Remove demo content from MainActivity

--- app/src/main/java/com/tbse/compose/todo/MainActivity.kt
+++ app/src/main/java/com/tbse/compose/todo/MainActivity.kt
@@ -25,17 +25,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeToDoTheme {
-
                 navController = rememberNavController()
                 SetupNavigation(navController = navController)
-
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
             }
         }
     }
